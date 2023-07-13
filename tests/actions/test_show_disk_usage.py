70e64fb8c6841c139093e3120a4b4bb40e6e0873
Jonathan <jonathankarlinski57@gmail.com>
2023-07-13 14:03:58 -0300

    refactor: Refatorando testes para função show_disk_usage

--- tests/actions/test_show_disk_usage.py
+++ tests/actions/test_show_disk_usage.py
@@ -1,28 +1,17 @@
-from pro_filer.actions.main_actions import show_disk_usage  # NOQA
 import pytest
+from pro_filer.actions.main_actions import show_disk_usage # NOQA
 from pro_filer.cli_helpers import _get_printable_file_path
 
 
 @pytest.fixture
-def context(tmp_path):
+def setup_files(tmp_path):
     file1 = tmp_path / "file1.txt"
     file2 = tmp_path / "file2.txt"
-    file3 = tmp_path / "file3.txt"
-    file1.write_text("Test file 1")
-    file2.write_text("Test file 2")
-    file3.write_text("Test file 3")
 
-    return {
-        "all_files": [str(file1), str(file2), str(file3)],
-    }
+    file1.write_text("This is file 1.")
+    file2.write_text("This is file 2." * 10)
 
-
-def test_show_disk_usage_empty(context, capsys):
-    context["all_files"] = []
-    show_disk_usage(context)
-    captured = capsys.readouterr()
-    output = captured.out.strip()
-    assert output == "Total size: 0"
+    return [str(file1), str(file2)]
 
 
 def test_show_disk_usage_with_files(setup_files, capfd):
