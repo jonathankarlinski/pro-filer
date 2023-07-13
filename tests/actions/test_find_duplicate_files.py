37b36eaa23c58067e4c96868b8a381089c653b0a
Jonathan <jonathankarlinski57@gmail.com>
2023-07-13 15:40:14 -0300

    test: Criando testes para função find_duplicate_files

--- tests/actions/test_find_duplicate_files.py
+++ tests/actions/test_find_duplicate_files.py
@@ -1,1 +1,61 @@
 from pro_filer.actions.main_actions import find_duplicate_files  # NOQA
+import os
+import pytest
+
+
+@pytest.fixture
+def create_temp_files(tmp_path):
+    file1 = tmp_path / "file1.txt"
+    file2 = tmp_path / "file2.txt"
+    file3 = tmp_path / "file3.txt"
+
+    file1.write_text("Content 1")
+    file2.write_text("Content 2")
+    file3.write_text("Content 1")
+
+    return str(tmp_path)
+
+
+def test_find_duplicate_files_no_duplicates(create_temp_files):
+    context = {
+        "all_files": [
+            os.path.join(create_temp_files, "file1.txt"),
+            os.path.join(create_temp_files, "file2.txt"),
+            os.path.join(create_temp_files, "file3.txt")
+        ]
+    }
+
+    duplicates = find_duplicate_files(context)
+    expected_duplicates = [
+        (
+            os.path.join(create_temp_files, "file1.txt"),
+            os.path.join(create_temp_files, "file3.txt")
+        )
+    ]
+    assert duplicates == expected_duplicates
+
+
+def test_find_duplicate_files_with_duplicates(create_temp_files):
+    context = {
+        "all_files": [
+            os.path.join(create_temp_files, "file1.txt"),
+            os.path.join(create_temp_files, "file2.txt"),
+            os.path.join(create_temp_files, "file3.txt"),
+        ]
+    }
+
+    duplicates = find_duplicate_files(context)
+    expected_duplicates = [
+        (
+            os.path.join(create_temp_files, "file1.txt"),
+            os.path.join(create_temp_files, "file3.txt"),
+        )
+    ]
+    assert duplicates == expected_duplicates
+
+
+def test_find_duplicate_files_missing_file():
+    context = {"all_files": ["file1.txt", "file2.txt", "file3.txt"]}
+
+    with pytest.raises(ValueError):
+        find_duplicate_files(context)
--- tests/actions/test_show_disk_usage.py
+++ tests/actions/test_show_disk_usage.py
@@ -1,33 +1,43 @@
 import pytest
-from pro_filer.actions.main_actions import show_disk_usage # NOQA
+from pro_filer.actions.main_actions import show_disk_usage  # NOQA
 from pro_filer.cli_helpers import _get_printable_file_path
 
 
 @pytest.fixture
-def setup_files(tmp_path):
-    file1 = tmp_path / "file1.txt"
-    file2 = tmp_path / "file2.txt"
-
+def context(tmp_path):
+    file1 = tmp_path / "app.py"
+    file2 = tmp_path / "__init__.py"
     file1.write_text("This is file 1.")
     file2.write_text("This is file 2." * 10)
 
-    return [str(file1), str(file2)]
-
-
-def test_show_disk_usage_with_files(setup_files, capfd):
-    context = {
-        "all_files": setup_files,
+    return {
+        "all_files": [
+            str(file1),
+            str(file2),
+        ]
     }
 
-    show_disk_usage(context)
 
-    captured = capfd.readouterr()
+def test_show_disk_usage_with_files(context, capsys):
+    show_disk_usage(context)
+    captured = capsys.readouterr()
     output = captured.out
 
+    teste = _get_printable_file_path(context['all_files'][1])
+    teste1 = _get_printable_file_path(context['all_files'][0])
     expected_output = (
-        f"'{_get_printable_file_path(setup_files[1])}':        150 (90%)\n"
-        f"'{_get_printable_file_path(setup_files[0])}':        15 (9%)\n"
+        f"'{teste}':        150 (90%)\n"
+        f"'{teste1}':        15 (9%)\n"
         "Total size: 165\n"
     )
 
     assert output == expected_output
+
+
+def test_show_disk_usage_without_files(context, capsys):
+    context["all_files"] = []
+    show_disk_usage(context)
+    captured = capsys.readouterr()
+    output = captured.out.strip().split("\n")
+
+    assert output[0] == "Total size: 0"
